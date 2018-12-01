ca3e4bcf14745169c7355e532533d03783b096ff
Antony Chan <antony.chan@oocl.com>
2018-12-02 01:48:16 +0800

    [Antony] test: add common method for uni tests

--- src/test/java/com/oocl/web/sampleWebApp/WebTestUtil.java
+++ src/test/java/com/oocl/web/sampleWebApp/WebTestUtil.java
@@ -1,11 +1,12 @@
 package com.oocl.web.sampleWebApp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oocl.web.sampleWebApp.domain.ParkingBoy;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.IOException;
 
-class WebTestUtil {
+public class WebTestUtil {
     public static <T> T toObject(String jsonContent, Class<T> clazz) throws IOException {
         final ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(jsonContent, clazz);
@@ -13,5 +14,13 @@ class WebTestUtil {
 
     public static <T> T getContentAsObject(MvcResult result, Class<T> clazz) throws Exception {
         return toObject(result.getResponse().getContentAsString(), clazz);
+    }
+
+    public static Long parseIdFromLocation(String location, String prefix){
+        return Long.valueOf(location.split(prefix)[1]);
+    }
+
+    public static String toJSON(ParkingBoy parkingBoy){
+        return "{\"employeeId\": " + "\"" + parkingBoy.getEmployeeId() + "\"" + "}";
     }
 }
