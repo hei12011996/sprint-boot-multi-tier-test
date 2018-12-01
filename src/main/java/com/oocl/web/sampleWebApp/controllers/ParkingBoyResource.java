656d3d89d04ebcf3f821b3d1d4dae62a5c7b1906
Antony Chan <antony.chan@oocl.com>
2018-12-02 01:55:01 +0800

    [Antony] feat: implement add() for ParkingBoy Resource

--- src/main/java/com/oocl/web/sampleWebApp/controllers/ParkingBoyResource.java
+++ src/main/java/com/oocl/web/sampleWebApp/controllers/ParkingBoyResource.java
@@ -1,12 +1,13 @@
 package com.oocl.web.sampleWebApp.controllers;
 
+import com.oocl.web.sampleWebApp.domain.ParkingBoy;
 import com.oocl.web.sampleWebApp.domain.ParkingBoyRepository;
 import com.oocl.web.sampleWebApp.models.ParkingBoyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/parkingboys")
@@ -21,5 +22,11 @@ public class ParkingBoyResource {
             .map(ParkingBoyResponse::create)
             .toArray(ParkingBoyResponse[]::new);
         return ResponseEntity.ok(parkingBoys);
+    }
+
+    @PostMapping
+    public ResponseEntity<ParkingBoyResponse> add(@RequestBody ParkingBoy parkingBoy) {
+        final ParkingBoy savedParkingBoy = parkingBoyRepository.saveAndFlush(parkingBoy);
+        return ResponseEntity.created(URI.create("/parkingboys/" + savedParkingBoy.getId())).build();
     }
 }
