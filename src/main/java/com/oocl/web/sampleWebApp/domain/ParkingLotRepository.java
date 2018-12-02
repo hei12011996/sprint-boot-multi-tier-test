ba6382acb0f3fcc02e2f04942b34d61031d1afd7
Antony Chan <antony.chan@oocl.com>
2018-12-02 15:44:24 +0800

    [Antony] feat: add ParkingLot repository

---
+++ src/main/java/com/oocl/web/sampleWebApp/domain/ParkingLotRepository.java
@@ -0,0 +1,10 @@
+package com.oocl.web.sampleWebApp.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
+    ParkingLot findByParkingLotId(String parkingLotId);
+}
+
