2d0ac740092ddfbc5ec497b1acd4b419474eb476
Antony Chan <antony.chan@oocl.com>
2018-12-02 17:29:35 +0800

    [Antony] feat: create model for ParkingBoyParkingLotsAssociationResponse

---
+++ src/main/java/com/oocl/web/sampleWebApp/models/ParkingBoyParkingLotsAssociationResponse.java
@@ -0,0 +1,49 @@
+package com.oocl.web.sampleWebApp.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.oocl.web.sampleWebApp.domain.ParkingBoy;
+import com.oocl.web.sampleWebApp.domain.ParkingLot;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ParkingBoyParkingLotsAssociationResponse {
+    private String employeeId;
+    private List<String> associatedParkingLots;
+
+    public static ParkingBoyParkingLotsAssociationResponse create(String employeeId, List<String> associatedParkingLots) {
+        Objects.requireNonNull(employeeId);
+        Objects.requireNonNull(associatedParkingLots);
+
+        final ParkingBoyParkingLotsAssociationResponse response = new ParkingBoyParkingLotsAssociationResponse();
+        response.setEmployeeId(employeeId);
+        response.setAssociatedParkingLots(associatedParkingLots);
+        return response;
+    }
+
+    public static ParkingBoyParkingLotsAssociationResponse create(ParkingBoy parkingBoy, List<ParkingLot> parkingLots) {
+        return create(parkingBoy.getEmployeeId(), parkingLots.stream().map(ParkingLot::getParkingLotId).collect(Collectors.toList()));
+    }
+
+    @JsonIgnore
+    public boolean isValid() {
+        return employeeId != null && associatedParkingLots != null;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public List<String> getAssociatedParkingLots() {
+        return associatedParkingLots;
+    }
+
+    private void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    private void setAssociatedParkingLots(List<String> associatedParkingLots) {
+        this.associatedParkingLots = associatedParkingLots;
+    }
+}
