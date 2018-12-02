8c3d82b707ead09751d2a04d6772570573087171
Antony Chan <antony.chan@oocl.com>
2018-12-02 17:54:06 +0800

    [Antony] test: add test cases for ParkingBoy - ParkingLots - Association

--- src/test/java/com/oocl/web/sampleWebApp/controllers/ParkingBoyResourceTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/controllers/ParkingBoyResourceTest.java
@@ -2,6 +2,9 @@ package com.oocl.web.sampleWebApp.controllers;
 
 import com.oocl.web.sampleWebApp.domain.ParkingBoy;
 import com.oocl.web.sampleWebApp.domain.ParkingBoyRepository;
+import com.oocl.web.sampleWebApp.domain.ParkingLot;
+import com.oocl.web.sampleWebApp.domain.ParkingLotRepository;
+import com.oocl.web.sampleWebApp.models.ParkingBoyParkingLotsAssociationResponse;
 import com.oocl.web.sampleWebApp.models.ParkingBoyResponse;
 import org.junit.After;
 import org.junit.Before;
@@ -31,12 +34,17 @@ public class ParkingBoyResourceTest {
     private ParkingBoyRepository parkingBoyRepository;
 
     @Autowired
+    private ParkingLotRepository parkingLotRepository;
+
+    @Autowired
     private MockMvc mvc;
 
     @Before
     public void clear_repository(){
         parkingBoyRepository.deleteAll();
         parkingBoyRepository.flush();
+        parkingLotRepository.deleteAll();
+        parkingLotRepository.flush();
     }
 
     @Test
@@ -145,5 +153,144 @@ public class ParkingBoyResourceTest {
         final List<ParkingBoy> parkingBoys = parkingBoyRepository.findAll();
 
         assertEquals(0, parkingBoys.size());
+    }
+
+    @Test
+    public void should_get_parking_boy_parking_lots_association_with_its_managing_parking_lot_id() throws Exception {
+        // Given
+        final ParkingBoy boy = parkingBoyRepository.save(new ParkingBoy("TEST CASE 6 Parking Boy"));
+        final Long pbId = boy.getId();
+        final ParkingLot lot = new ParkingLot("TEST CASE 6 Parking Lot", 100);
+        lot.setParkingBoyId(pbId);
+        parkingLotRepository.saveAndFlush(lot);
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/parkingboys/" + pbId + "/parkinglots"))
+                .andReturn();
+
+        // Then
+        assertEquals(200, result.getResponse().getStatus());
+
+        final ParkingBoyParkingLotsAssociationResponse association = getContentAsObject(result, ParkingBoyParkingLotsAssociationResponse.class);
+
+        assertEquals("TEST CASE 6 Parking Boy", association.getEmployeeId());
+        assertEquals("TEST CASE 6 Parking Lot 1", association.getAssociatedParkingLots().get(0));
+    }
+
+    @Test
+    public void should_get_parking_boy_parking_lots_association_with_multiple_parking_lot_id() throws Exception {
+        // Given
+        final ParkingBoy boy = parkingBoyRepository.save(new ParkingBoy("TEST CASE 7 Parking Boy"));
+        final Long pbId = boy.getId();
+        final ParkingLot lot_1 = new ParkingLot("TEST CASE 7 Parking Lot 1", 100);
+        lot_1.setParkingBoyId(pbId);
+        final ParkingLot lot_2 = new ParkingLot("TEST CASE 7 Parking Lot 2", 100);
+        lot_2.setParkingBoyId(pbId);
+        parkingLotRepository.save(lot_1);
+        parkingLotRepository.saveAndFlush(lot_2);
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/parkingboys/" + pbId + "/parkinglots"))
+                .andReturn();
+
+        // Then
+        assertEquals(200, result.getResponse().getStatus());
+
+        final ParkingBoyParkingLotsAssociationResponse association = getContentAsObject(result, ParkingBoyParkingLotsAssociationResponse.class);
+
+        assertEquals("TEST CASE 7 Parking Boy", association.getEmployeeId());
+        assertEquals("TEST CASE 7 Parking Lot 1", association.getAssociatedParkingLots().get(0));
+        assertEquals("TEST CASE 7 Parking Lot 2", association.getAssociatedParkingLots().get(1));
+    }
+
+    @Test
+    public void should_get_parking_boy_parking_lots_association_with_zero_parking_lot_id() throws Exception {
+        // Given
+        final ParkingBoy boy = parkingBoyRepository.saveAndFlush(new ParkingBoy("TEST CASE 8 Parking Boy"));
+        final Long pbId = boy.getId();
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/parkingboys/" + pbId + "/parkinglots"))
+                .andReturn();
+
+        // Then
+        assertEquals(200, result.getResponse().getStatus());
+
+        final ParkingBoyParkingLotsAssociationResponse association = getContentAsObject(result, ParkingBoyParkingLotsAssociationResponse.class);
+
+        assertEquals("TEST CASE 8 Parking Boy", association.getEmployeeId());
+        assertTrue(association.getAssociatedParkingLots().isEmpty());
+    }
+    @Test
+    public void should_not_get_parking_boy_parking_lots_association_when_parking_boy_not_exist() throws Exception {
+        // Given
+        final Long pbId = 0L;
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/parkingboys/" + pbId + "/parkinglots"))
+                .andReturn();
+
+        // Then
+        assertEquals(404, result.getResponse().getStatus());
+    }
+
+    @Test
+    public void should_save_parking_boy_parking_lots_association() throws Exception {
+        // Given
+        final ParkingBoy boy = parkingBoyRepository.save(new ParkingBoy("TEST CASE 10 Parking Boy"));
+        final Long pbId = boy.getId();
+        final ParkingLot lot = parkingLotRepository.save(new ParkingLot("TEST CASE 10 Parking Lot", 100));
+        final Long plId = lot.getId();
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .post("/parkingboys/" + pbId + "/parkinglots/" + plId))
+                .andReturn();
+
+        // Then
+        final String location = result.getResponse().getHeader("location");
+
+        assertEquals(201, result.getResponse().getStatus());
+        assertTrue(location.contains("/parkingboys/"));
+
+        final ParkingLot updatedParkingLot = parkingLotRepository.findByParkingLotId("TEST CASE 10 Parking Lot");
+
+        assertEquals(pbId, updatedParkingLot.getParkingBoyId());
+    }
+
+    @Test
+    public void should_not_save_parking_boy_parking_lots_association_given_none_existing_parking_boy_id() throws Exception {
+        // Given
+        final Long pbId = 0L;
+        final ParkingLot lot = parkingLotRepository.save(new ParkingLot("TEST CASE 11 Parking Lot", 100));
+        final Long plId = lot.getId();
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .post("/parkingboys/" + pbId + "/parkinglots/" + plId))
+                .andReturn();
+
+        // Then
+        assertEquals(400, result.getResponse().getStatus());
+    }
+
+    @Test
+    public void should_not_save_parking_boy_parking_lots_association_given_none_existing_parking_lot_id() throws Exception {
+        // Given
+        final ParkingBoy boy = parkingBoyRepository.save(new ParkingBoy("TEST CASE 12 Parking Boy"));
+        final Long pbId = boy.getId();
+        final Long plId = 0L;
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .post("/parkingboys/" + pbId + "/parkinglots/" + plId))
+                .andReturn();
+
+        // Then
+        assertEquals(400, result.getResponse().getStatus());
     }
 }
